344b9631bc75d3b08a4532f7d0cd19cb93dc199f
Yusuf Karakaya <yusufkarakaya92@gmail.com>
2024-10-15 15:50:28 -0700

    handle case where API returns video instead of image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
-const Main = (props) => {
-  const { data } = props;
+const Main = ({ data }) => {
   return (
-    <div className="imgContainer">
-      <img
-        className="bgImage"
-        src={data.hdurl}
-        alt={data.title || "bg-image"}
-      />
+    <div>
+      {data.media_type === "video" ? (
+        <iframe className="video" src={data.url} frameborder="0"></iframe>
+      ) : (
+        <div className="imgContainer">
+          <img
+            className="bgImage"
+            src={data.hdurl}
+            alt={data.title || "bg-image"}
+          />
+        </div>
+      )}
     </div>
   );
 };
